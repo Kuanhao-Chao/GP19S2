4f826ec2ca4d32ea74c66a673ac87ae51217bb55
Michael Betterton <u6797866@anu.edu.au>
2019-09-05 15:48:38 +1000

    Change FunctionParser test constructor move javadocs to correct place and unnecessary verbosity.

--- app/src/test/java/com/anu/calculator/FunctionParserTest.java
+++ app/src/test/java/com/anu/calculator/FunctionParserTest.java
@@ -19,23 +19,17 @@ public class FunctionParserTest {
      */
     private class TestCase {
 
-        /**
-         * The default constructor for a test case.
-         *
-         * @param input The input function as a string.
-         * @param twoExpression After the function is created, two expressions on each side of '='
-         *                      will be stored in twoExpression.
-         * @param validityExpected Stored the expected answer whether function is valid.
-         */
         String input;
         List<String> twoExpression;
         Boolean validityExpected;
 
         /**
          * The default constructor for a test case.
-         *
-         * @param input    The input expression as a string. It should conform to the known tokens of
-         *                 the application. Token parsing is tested in a different test suite.
+         *expected
+         * @param input The input function as a string.
+         * @param twoExpression After the function is created, two expressions on each side of '='
+         *                      will be stored in twoExpression.
+         * @param expected Stored the expected answer whether function is valid.
          */
         TestCase(String input, List<String> twoExpression, Boolean expected) {
             this.input = input;
@@ -53,40 +47,37 @@ public class FunctionParserTest {
     @Test
     public void testFunctionResult() {
         // Declare each of the test cases
-        ArrayList<FunctionParserTest.TestCase> testCases = new ArrayList<>(0);
-        testCases.add(new FunctionParserTest.TestCase("", null,false));
-        testCases.add(new FunctionParserTest.TestCase(" ", null,false));
-        testCases.add(new FunctionParserTest.TestCase("   ", null,false));
-        testCases.add(new FunctionParserTest.TestCase("=", null,false));
-        testCases.add(new FunctionParserTest.TestCase("==", null,false));
-        testCases.add(new FunctionParserTest.TestCase("a", null,false));
-        testCases.add(new FunctionParserTest.TestCase("a=", null,false));
-        testCases.add(new FunctionParserTest.TestCase("a==", null,false));
-        testCases.add(new FunctionParserTest.TestCase("=a", null,false));
-        testCases.add(new FunctionParserTest.TestCase("==a", null,false));
-        testCases.add(new FunctionParserTest.TestCase("x=y=x", null,false));
-        testCases.add(new FunctionParserTest.TestCase("x========yz", null,false));
-        testCases.add(new FunctionParserTest.TestCase("x====y====z", null,false));
-        testCases.add(new FunctionParserTest.TestCase("x=y=z=w=ɑ=β", null,false));
-        testCases.add(new FunctionParserTest.TestCase("x==y", null,false));
-        testCases.add(new FunctionParserTest.TestCase("=x=y", null,false));
-        testCases.add(new FunctionParserTest.TestCase("x=y=", null,false));
-        testCases.add(new FunctionParserTest.TestCase("=x=y=", null,false));
+        ArrayList<TestCase> testCases = new ArrayList<>(0);
+        testCases.add(new TestCase("", null,false));
+        testCases.add(new TestCase(" ", null,false));
+        testCases.add(new TestCase("   ", null,false));
+        testCases.add(new TestCase("=", null,false));
+        testCases.add(new TestCase("==", null,false));
+        testCases.add(new TestCase("a", null,false));
+        testCases.add(new TestCase("a=", null,false));
+        testCases.add(new TestCase("a==", null,false));
+        testCases.add(new TestCase("=a", null,false));
+        testCases.add(new TestCase("==a", null,false));
+        testCases.add(new TestCase("x=y=x", null,false));
+        testCases.add(new TestCase("x========yz", null,false));
+        testCases.add(new TestCase("x====y====z", null,false));
+        testCases.add(new TestCase("x=y=z=w=ɑ=β", null,false));
+        testCases.add(new TestCase("x==y", null,false));
+        testCases.add(new TestCase("=x=y", null,false));
+        testCases.add(new TestCase("x=y=", null,false));
+        testCases.add(new TestCase("=x=y=", null,false));
         //        w | x | y | z | ɑ | β | ɣ | Δ
-        testCases.add(new FunctionParserTest.TestCase("2+1×2÷3×12 = 9124", Arrays.asList("2+1×2÷3×12", "9124"),true));
-        testCases.add(new FunctionParserTest.TestCase("2+1×2÷3×w = 9124", Arrays.asList("2+1×2÷3×w", "9124"),true));
-        testCases.add(new FunctionParserTest.TestCase("w+1×3÷3×12 = 9124", Arrays.asList("w+1×3÷3×12", "9124"),true));
-        testCases.add(new FunctionParserTest.TestCase("w+x×y÷z×ɑ = 9124", Arrays.asList("w+x×y÷z×ɑ", "9124"),true));
-        testCases.add(new FunctionParserTest.TestCase("x=y", Arrays.asList("x", "y"),true));
-        testCases.add(new FunctionParserTest.TestCase("x=y    ", Arrays.asList("x", "y"),true));
-        testCases.add(new FunctionParserTest.TestCase("    x=y", Arrays.asList("x", "y"),true));
-        testCases.add(new FunctionParserTest.TestCase("x    =y", Arrays.asList("x", "y"),true));
-        testCases.add(new FunctionParserTest.TestCase("x=     y", Arrays.asList("x", "y"),true));
-        testCases.add(new FunctionParserTest.TestCase("x=     y", Arrays.asList("x", "y"),true));
-        testCases.add(new FunctionParserTest.TestCase("1×w + 2×x + 3×y + 4×z + 5×ɑ + 6×β + 7×ɣ + 8×Δ =    1÷w + 2÷x + 3÷y + 4÷z + 5÷ɑ + 6÷β + 7÷ɣ + 8÷Δ", Arrays.asList("1×w+2×x+3×y+4×z+5×ɑ+6×β+7×ɣ+8×Δ", "1÷w+2÷x+3÷y+4÷z+5÷ɑ+6÷β+7÷ɣ+8÷Δ"),true));
-
-
-
+        testCases.add(new TestCase("2+1×2÷3×12 = 9124", Arrays.asList("2+1×2÷3×12", "9124"),true));
+        testCases.add(new TestCase("2+1×2÷3×w = 9124", Arrays.asList("2+1×2÷3×w", "9124"),true));
+        testCases.add(new TestCase("w+1×3÷3×12 = 9124", Arrays.asList("w+1×3÷3×12", "9124"),true));
+        testCases.add(new TestCase("w+x×y÷z×ɑ = 9124", Arrays.asList("w+x×y÷z×ɑ", "9124"),true));
+        testCases.add(new TestCase("x=y", Arrays.asList("x", "y"),true));
+        testCases.add(new TestCase("x=y    ", Arrays.asList("x", "y"),true));
+        testCases.add(new TestCase("    x=y", Arrays.asList("x", "y"),true));
+        testCases.add(new TestCase("x    =y", Arrays.asList("x", "y"),true));
+        testCases.add(new TestCase("x=     y", Arrays.asList("x", "y"),true));
+        testCases.add(new TestCase("x=     y", Arrays.asList("x", "y"),true));
+        testCases.add(new TestCase("1×w + 2×x + 3×y + 4×z + 5×ɑ + 6×β + 7×ɣ + 8×Δ =    1÷w + 2÷x + 3÷y + 4÷z + 5÷ɑ + 6÷β + 7÷ɣ + 8÷Δ", Arrays.asList("1×w+2×x+3×y+4×z+5×ɑ+6×β+7×ɣ+8×Δ", "1÷w+2÷x+3÷y+4÷z+5÷ɑ+6÷β+7÷ɣ+8÷Δ"),true));
 
         // Run each test case programmatically by looping over the cases
         int counter = 0;
